6fce7bb25556203553ac348d068ca3ab00394a1b
kenghoufong1 <kenghoufong1@gmail.com>
2023-02-01 23:21:11 -0800

    fixed all file errors and everything works

--- Develop/server.js
+++ Develop/server.js
@@ -5,16 +5,15 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(__dirname + 'public'));
-
+app.use(express.static(__dirname + '/public'));
 app.use(express.json());
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'notes.html'))
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
@@ -49,13 +48,14 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-app.delete('/api/notes:id', (req, res) => {
+app.delete('/api/notes/:id', (req, res) => {
   fs.readFile('db.json', (err, data) => {
     if (err) {
       res.status(500).send({ error: 'Error reading db.json file' });
     } else {
-      const notes = JSON.parse(data);
-      notes = notes.filter(note => note.id !== req.params.id);
+      let notes = JSON.parse(data);
+      notes = notes.filter(note => note.id !== Number(req.params.id));
+      console.log(notes);
       fs.writeFile('db.json', JSON.stringify(notes), (err) => {
         if (err) {
           res.status(500).send({ error: 'Error writing to db.json file' });
