064dc4162baf772dc5076d6ac30333ec62353ef9
kenghoufong1 <kenghoufong1@gmail.com>
2023-02-02 22:30:37 -0800

    Revert "added final changes to all file besides the readme.md"
    This reverts commit 33edca573775108ac080eb4a7cbd80145ce60229.

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { log } = require('console');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -36,7 +35,6 @@ app.post('/api/notes', (req, res) => {
     } else {
       const notes = JSON.parse(data);
       const newNote = req.body;
-      // console.log(newNote);
       newNote.id = notes.length + 1;
       notes.push(newNote);
       fs.writeFile('db.json', JSON.stringify(notes), (err) => {
@@ -51,14 +49,14 @@ app.post('/api/notes', (req, res) => {
 });
 
 
-app.delete('/api/notes/:id', (req, res) => {
+app.delete('/api/notes:id', (req, res) => {
   fs.readFile('db.json', (err, data) => {
     if (err) {
       res.status(500).send({ error: 'Error reading db.json file' });
     } else {
-      let notes = JSON.parse(data);
-      notestemp = notes.filter(note => note.id !== Number(req.params.id));
-      fs.writeFile('db.json', JSON.stringify(notestemp), (err) => {
+      const notes = JSON.parse(data);
+      notes = notes.filter(note => note.id !== req.params.id);
+      fs.writeFile('db.json', JSON.stringify(notes), (err) => {
         if (err) {
           res.status(500).send({ error: 'Error writing to db.json file' });
         } else {
